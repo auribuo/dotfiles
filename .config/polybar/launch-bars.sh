7dd4bd46ad3d32d243c969492838d4b53abe5e88
Aurelio Buonomo <auribuo@gmail.com>
2022-12-10 10:47:30 +0100

    update polybar

--- .config/polybar/launch-bars.sh
+++ .config/polybar/launch-bars.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [[ $(xrandr -q | grep "HDMI-1-0 connected") ]]; then
-    polybar wm-lg-top & polybar wm-lg-bl & polybar wm-lg-bc & polybar wm-lg-br & polybar wm-sm-top
+    polybar -l warning wm-lg-top & polybar -l warning wm-lg-bottom & polybar -l warning wm-sm-top &
 else
-    polybar nm-sm-top & polybar nm-sm-bl & polybar nm-sm-bc & polybar nm-sm-br
+    polybar -l warning nm-sm-top & polybar -l warning nm-sm-bottom &
 fi
